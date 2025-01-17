cfdd3edc29bc8e4b850a088b125fc4d8ea2114dc
Morten Solberg <morten@source-code.no>
2025-01-17 08:18:21 +0100

    FKS-1048: Library for audit.

---
+++ src/main/java/no/fintlabs/audit/AuditEntity.java
@@ -0,0 +1,28 @@
+package no.fintlabs.audit;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class AuditEntity {
+    @CreatedDate
+    private Instant createdDate;
+
+    @LastModifiedDate
+    private Instant modifiedDate;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedBy
+    private String modifiedBy;
+}
+
