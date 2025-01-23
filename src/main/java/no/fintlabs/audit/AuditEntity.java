c6bdf3835aecd574aaabe20d032568cd83a76fc7
Morten Solberg <morten@source-code.no>
2025-01-23 13:02:12 +0100

    FKS-1048: Remove abstract

--- src/main/java/no/fintlabs/audit/AuditEntity.java
+++ src/main/java/no/fintlabs/audit/AuditEntity.java
@@ -14,7 +14,7 @@ import java.time.Instant;
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @SuperBuilder
-public abstract class AuditEntity {
+public class AuditEntity {
     @CreatedDate
     private Instant createdDate;
 
